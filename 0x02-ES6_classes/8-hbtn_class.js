b7432d26c0621c12b3dcbb37f0a93546efb11129
bolexs <boluwatufeadeojo@gmail.com>
2024-01-05 22:43:31 +0100

    hbn class

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,19 @@
+class HolbertonClass {
+    constructor(size, location) {
+      this._size = size;
+      this._location = location;
+    }
+  
+    [Symbol.toPrimitive](hint) {
+      if (hint === 'number') {
+        return this._size;
+      }
+      if (hint === 'string') {
+        return this._location;
+      }
+      return null;
+    }
+  }
+  
+  module.exports = HolbertonClass;
+  
