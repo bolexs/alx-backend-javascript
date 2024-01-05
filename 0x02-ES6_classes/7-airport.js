4d42384727f7581b43905fbf6ad2523c550c4467
bolexs <boluwatufeadeojo@gmail.com>
2024-01-05 22:41:19 +0100

    airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,13 @@
+class Airport {
+    constructor(name, code) {
+      this.name = name;
+      this.code = code;
+    }
+  
+    get [Symbol.toStringTag]() {
+      return this.code;
+    }
+  }
+  
+  module.exports = Airport;
+  
