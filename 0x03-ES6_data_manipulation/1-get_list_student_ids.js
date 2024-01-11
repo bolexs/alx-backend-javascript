cf8212a982190744e603439346574293e721661d
bolexs <boluwatufeadeojo@gmail.com>
2024-01-11 01:20:35 +0100

    workings

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+const getListStudentIds = (array) => {
+    if (!Array.isArray(array)) {
+      return [];
+    }
+    return array.map((student) => student.id);
+  };
+  
+  module.exports = getListStudentIds;
+  
